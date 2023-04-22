40a545df16809d9d1e29192e4feea4a18cbae899
humbre <leila.roserosette@outlook.fr>
2023-04-22 19:31:44 +0200

    Affichage page accueil + css

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,54 +1,59 @@
 class Photographer {
     constructor(name, id, city, country, tagline, price, portrait) {
-        this._name = name;
-        this._id = id;
-        this._city = city;
-        this._country = country;
-        this._tagline = tagline;
-        this._price = price;
-        this._portrait = portrait;
+      this._name = name;
+      this._id = id;
+      this._city = city;
+      this._country = country;
+      this._tagline = tagline;
+      this._price = price;
+      this._portrait = portrait;
     }
-
+  
     // Méthode pour afficher les informations du photographe
     displayInfo() {
-        console.log(`Name: ${this._name}, City: ${this._city}, Country: ${this._country}, Tagline: ${this._tagline}, Price: ${this._price}, Portrait: ${this._portrait}`);
+      console.log(`Name: ${this._name}, City: ${this._city}, Country: ${this._country}, Tagline: ${this._tagline}, Price: ${this._price}, Portrait: ${this._portrait}`);
     }
-}
+  }
+  
 
-async function getData() {
-    try {
-        // Récupération des données du fichier JSON local
-        const response = await fetch('../../../data/photographers.json');
-        const data = await response.json();
+  function getData() {
+    return fetch('../../../data/photographers.json')
+      .then(response => response.json())
+      .then(data => {
         // et bien retourner le tableau photographers
-        displayData(data.photographers);
-        console.log(data.photographers);
-    } catch (error) {
+        return data.photographers;
+      })
+      .catch(error => {
         console.error(error);
-    }
-}
-
-// Affichage des informations de chaque photographe
-async function displayData(photographers) {
-    const photographersSection = document.querySelector(".photographer_section");
-    photographers.forEach((photographer) => {
-        const photographerModel = new Photographer(
-            photographer.name,
-            photographer.id,
-            photographer.city,
-            photographer.country,
-            photographer.tagline,
-            photographer.price,
-            photographer.portrait
-        );
-        photographerModel.displayInfo();
-        const userCardDOM = photographerModel.getUserCardDOM();
-        photographersSection.appendChild(userCardDOM);
+      });
+  }
+  
+  // Affichage des informations de chaque photographe
+  function displayData(photographers) {
+    const photographersSection = document.querySelector('.photographer_section');
+    photographers.forEach(photographer => {
+      const photographerModel = new Photographer(
+        photographer.name,
+        photographer.id,
+        photographer.city,
+        photographer.country,
+        photographer.tagline,
+        photographer.price,
+        photographer.portrait
+      );
+      photographerModel.displayInfo();
+      const userCard = photographerFactory(photographer);
+      const userCardDOM = userCard.getUserCardDOM();
+      photographersSection.appendChild(userCardDOM);
     });
-}
-
-async function init() {
-    await getData();
-}
-
-init();
+  }
+  
+  function init() {
+    getData().then(photographers => {
+      displayData(photographers);
+      console.log(photographers);
+    });
+  }
+  
+  init();
+  
