a78ddf4234f21808ba47bf7786f8b180a3cac487
humbre <leila.roserosette@outlook.fr>
2023-04-25 23:05:37 +0200

    Style, modale et access + début articles détaillés

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,1 +1,21 @@
-//Mettre le code JavaScript lié à la page photographer.html
+const info = document.getElementById('info');
+const imgPhotographer = document.getElementById('imgPhotographer');
+const photographerSection = document.querySelector('.photographer_section');
+const portfolio = document.getElementById('portfolio');
+
+function displayElement(dataPhotographer) {
+  photographerSection.addEventListener('click', (event) => {
+    const article = event.target.closest('article');
+    if (article) {
+      info.textContent = `${photographer.name}, ${photographer.city}, ${photographer.country}, ${photographer.tagline}, ${photographer.price}`;
+      imgPhotographer.src = `assets/images/${photographer.portrait}`;
+      portfolio.innerHTML = '';
+      photographer.portfolio.forEach(picture => {
+        const pictureElement = document.createElement('img');
+        pictureElement.src = `assets/images/${picture}`;
+        pictureElement.alt = '';
+        portfolio.appendChild(pictureElement);
+      });
+    }
+  });
+}
--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,9 +1,33 @@
-function displayModal() {
-    const modal = document.getElementById("contact_modal");
-	modal.style.display = "block";
+
+//const formData = document.getElementById("contact_modal"); 
+//const btnSubmit = document.querySelector('.displayModal');
+const openModal = document.getElementById('contact_modal'); //le formulaire
+const btnsendData = document.querySelector('.sendData'); //bouton de modale pour envoyer les données
+
+//fonction d'ouverture de la modale
+
+function displayModal(){
+    openModal.style.display = "block";
 }
 
-function closeModal() {
-    const modal = document.getElementById("contact_modal");
-    modal.style.display = "none";
+//fonction de fermeture de la modale
+
+function closeModal(){
+    openModal.style.display = "none";
+}
+
+//Envoyer les données
+btnsendData.addEventListener("click", sendData);
+
+function sendData(){
+    alert('Merci, le photographe reviendra vers vous dans les plus brefs délais');
+    closeModal();
+   
+//    if (formData.firstName === ''||formData.lastName === '' || formData.email === '' || formData.message === '') {
+//        alert('Il y a eu une erreur, veuillez réessayer');
+//    } else{
+//        alert('Merci, le photographe reviendra vers vous dans les plus brefs délais');
+//        form.reset();
+//        closeModal();
+//    }
 }
