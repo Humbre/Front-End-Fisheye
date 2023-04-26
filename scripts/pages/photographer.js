46f5be41cd195d77cdbe5e956182b138636ce764
humbre <leila.roserosette@outlook.fr>
2023-04-26 17:57:37 +0200

    displayinfo()

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -2,20 +2,27 @@ const info = document.getElementById('info');
 const imgPhotographer = document.getElementById('imgPhotographer');
 const photographerSection = document.querySelector('.photographer_section');
 const portfolio = document.getElementById('portfolio');
-
+ 
 function displayElement(dataPhotographer) {
-  photographerSection.addEventListener('click', (event) => {
-    const article = event.target.closest('article');
-    if (article) {
-      info.textContent = `${photographer.name}, ${photographer.city}, ${photographer.country}, ${photographer.tagline}, ${photographer.price}`;
-      imgPhotographer.src = `assets/images/${photographer.portrait}`;
-      portfolio.innerHTML = '';
-      photographer.portfolio.forEach(picture => {
-        const pictureElement = document.createElement('img');
-        pictureElement.src = `assets/images/${picture}`;
-        pictureElement.alt = '';
-        portfolio.appendChild(pictureElement);
-      });
-    }
-  });
+    let selectedPhotographer = {}; // variable pour stocker le photographe sélectionné
+  
+    photographerSection.addEventListener('click', (event) => {
+        const article = event.target.closest('article');
+        if (article) {
+            selectedPhotographer = dataPhotographer;
+            info.textContent = `${selectedPhotographer.name}, ${selectedPhotographer.city}, ${selectedPhotographer.country}, ${selectedPhotographer.tagline}, ${selectedPhotographer.price}`;
+            imgPhotographer.src = `assets/images/${selectedPhotographer.portrait}`;
+            portfolio.innerHTML = '';
+            selectedPhotographer.portfolio.forEach(picture => {
+                const pictureElement = document.createElement('img');
+                pictureElement.src = `assets/images/${picture}`;
+                //pictureElement.alt = '';
+                portfolio.appendChild(pictureElement);
+            });
+        } else {
+            
+        }
+    });
+    
+    return selectedPhotographer; 
 }
