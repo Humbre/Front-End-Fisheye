09a11b195fe39f4a635555ce23fce249351f0b4e
humbre <leila.roserosette@outlook.fr>
2023-05-03 16:16:47 +0200

    photographer.js

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,55 +1,53 @@
-// Affichage de la page photographers.html
-//displayData(photographers);
+// Récupération des éléments du DOM
+const titleElement = document.querySelector('title');
+const photographerNameElement = document.getElementById('info');
+const profilePictureElement = document.getElementById('imgPhotographer');
+const portfolioElement = document.querySelector('#portfolio');
 
-// récupère les ID de tous les photographes
-const photographerIds = photographers.map(photographer => photographer.id);
+// Récupération des données du photographe via Fetch
+fetch('../../../data/photographers.json')
+  .then(response => response.json())
+  .then(data => {
+    // Récupération des données du photographe
+    const photographer = data.photographers.find(p => p.id === photographerId);
 
-// enregistre l'ID du photographe sélectionné dans l'URL
-const urlParams = new URLSearchParams(window.location.search);
-urlParams.set('id', selectedPhotographerId);
-window.history.pushState({}, '', `${window.location.pathname}?${urlParams}`);
+    // Mise à jour du contenu du DOM
+    titleElement.textContent = photographer.name;
+    photographerNameElement.textContent = photographer.name + `, ${photographer.city}, ${photographer.country}`;
+    profilePictureElement.src = `assets/images/photographers/${photographer.portrait}`;
+    profilePictureElement.alt = photographer.alt;
 
-// récupère les paramètres de l'URL dans une constante
-const params = new URLSearchParams(window.location.search);
-const photographerId = parseInt(params.get('id'));
-
-// vérifie que l'ID du photographe est valide
-if (!photographerIds.includes(photographerId)) {
-  console.error('Invalid photographer ID');
-}
-
-// Affichage de toutes les photos de chaque photographe grace à l'ID qui est dans l'URL
-function displayElement(photographerId) {
-    const portfolio = document.querySelector('#portfolio');
-    const photographer = photographers.find(p => p.id === photographerId);
+    // Affichage des images du portfolio du photographe
     photographer.portfolio.forEach(picture => {
       const pictureElement = document.createElement('img');
-      pictureElement.src = `assets/images/${photographerId}`;
-      pictureElement.alt = '';
-      portfolio.appendChild(pictureElement);
+      pictureElement.src = `assets/images/${photographerId}/${picture.image}`;
+      pictureElement.alt = picture.alt;
+      portfolioElement.appendChild(pictureElement);
     });
+  })
+  .catch(error => console.error(error));
+
+//Affichage photo actuelle
+let currentPictureIndex = 0;
+const pictures = ["picture1.jpg", "picture2.jpg", "picture3.jpg"];
+
+
+const displayCurrentPicture = () => {
+  const currentPicture = pictures[currentPictureIndex]; // récupère la photo courante
+  portfolioElement.src = currentPicture; // affiche la photo courante
+};
+
+// Droite gauche avec le clavier
+displayCurrentPicture();
+document.addEventListener('keydown', (event) => {
+  const key = event.key;
+  if (key === 'ArrowLeft') {
+    currentPictureIndex = (currentPictureIndex - 1 + pictures.length) % pictures.length; // calcule l'index de la photo précédente
+    displayCurrentPicture(); // affiche la photo précédente
+  } else if (key === 'ArrowRight') {
+    currentPictureIndex = (currentPictureIndex + 1) % pictures.length; // calcule l'index de la photo suivante
+    displayCurrentPicture(); // affiche la photo suivante
   }
+});
 
-  displayElement();
-
-  //Affichage photo actuelle
-  const displayCurrentPicture = () => {
-    const currentPicture = pictures[currentPictureIndex]; // récupère la photo courante
-    picture.src = currentPicture; // affiche la photo courante
-  };
-  
-
-  // Droite gauche avec le clavier
-  displayCurrentPicture();
-  document.addEventListener('keydown', (event) => {
-    const key = event.key;
-    if (key === 'ArrowLeft') {
-        currentPictureIndex = (currentPictureIndex - 1 + pictures.length); // calcule l'index de la photo précédente
-        displayCurrentPicture(); // affiche la photo précédente
-    } else if (key === 'ArrowRight') {
-        currentPictureIndex = (currentPictureIndex + 1) ; // calcule l'index de la photo suivante
-        displayCurrentPicture(); // affiche la photo suivante
-    }
-  });
-  
-  displayCurrentPicture();
+displayCurrentPicture();
